91dd845bca6485b9cb0564cf304566d3cff43933
mac <2961070026@qq.com>
2024-04-05 23:28:46 +0800

    project contact

---
+++ Project/contact_cplusplus/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  contact_cplusplus
+//
+//  Created by mac on 2024/4/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
